13ddd25b6de84db70a462ad078a941f43507a45f
akuppili45 <adityakuppili@Adityas-MBP-2.attlocal.net>
2023-07-16 12:32:53 -0700

    removed stored_docs

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,6 @@ from llama_index import SimpleDirectoryReader, VectorStoreIndex, StorageContext,
 from flask import Flask, request, make_response, jsonify
 import boto3
 import s3fs
-from s3reader import S3DirectoryReader
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
 import json
@@ -22,11 +21,6 @@ bucket = s3.Bucket('llm-indices-1')
 s3filesys = s3fs.S3FileSystem(
    anon=False
 )
-stored_docs = {}
-# for obj in bucket.objects.filter(Prefix='documents/'):
-#     idx = obj.key.index('/')
-#     print(obj.key[idx + 1:])
-    # print(obj.key[obj.key.find('documents/') + len(obj.key):])
 
 def IsObjectExists(path):
     for object_summary in bucket.objects.filter(Prefix=path):
@@ -36,7 +30,6 @@ def initialize_index():
     global index
     storage_context = StorageContext.from_defaults()
     service_context = ServiceContext.from_defaults(chunk_size_limit=512)
-    print(IsObjectExists('storage_demo/'))
     if IsObjectExists('storage_demo/'):
         index = load_index_from_storage(StorageContext.from_defaults(persist_dir='llm-indices-1/storage_demo',fs=s3filesys), service_context=service_context)
     else:
@@ -72,9 +65,7 @@ def get_documents():
     for obj in bucket.objects.filter(Prefix='documents/'):
         idx = obj.key.index('/')
         document_list.append(obj.key[idx + 1:])
-    print(document_list)
     return make_response(jsonify(document_list)), 200
-    # return []
 
 @app.route("/uploadFile", methods=["GET","POST"])
 def uploadFile():
@@ -85,25 +76,16 @@ def uploadFile():
     try:
         uploaded_file = request.files["file"]
         uploaded_file.filename = secure_filename(uploaded_file.filename)
-        print("before file upload", flush=True)
         s3_client.upload_fileobj(uploaded_file, 'llm-indices-1', 'documents/'+ uploaded_file.filename, ExtraArgs={
             "ContentType": uploaded_file.content_type    #Set appropriate content type as per the file
-        })
-        print("after file upload", flush=True)
-        # filepath = os.path.join('documents', os.path.basename(uploaded_file.filename))
-        # print(type(filepath))
-        # print(filepath)
-        # # uploaded_file.save(filepath)
-        # # put into s3 documents/
-        
+        })        
         if request.form.get("filename_as_doc_id", None) is not None:
-            print('not none')
             insert_into_index('documents/'+ uploaded_file.filename, doc_id=uploaded_file.filename)
         else:
             insert_into_index('documents/'+ uploaded_file.filename)
     except Exception as e:
-        print(e)
         # cleanup temp file
+        print(e)
         if filepath is not None and os.path.exists(filepath):
             os.remove(filepath)
         return "Error: {}".format(str(e)), 500
@@ -112,13 +94,12 @@ def uploadFile():
 def insert_into_index(doc_file_path, doc_id=None):
     global index, stored_docs
     S3Reader = download_loader("S3Reader")
-    # print(doc_file_path)
     loader = S3Reader(bucket='llm-indices-1', key=doc_file_path)
     document = loader.load_data()[0]
     if doc_id is not None:
         document.doc_id = doc_id
     
-    stored_docs[document.doc_id] = document.text[0:200]
+    # stored_docs[document.doc_id] = document.text[0:200]
     index.insert(document)
     index.storage_context.persist('llm-indices-1/storage_demo', fs=s3filesys)
 
